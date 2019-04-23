dffc5f352a262da139fb23419a65e18165e925ad
Alyssa Wei <wei.alyssa@gmail.com>
2019-04-22 19:25:23 -0700

    Passing most tests for VideoListEntry

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,18 @@
-var VideoList = () => (
+import VideoListEntry from "./VideoListEntry";
+
+var VideoList = (props) => (
   <div className="video-list">
+
+    {props.videos.map(video =>
+    // console.log(video);
+      <VideoListEntry video={video} />
+    )}
+
+    {/* <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    <div><h5><em>videoListEntry</em> view goes here</h5></div> */}
   </div>
 );
 
